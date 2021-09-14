9153b51cfb48bae28acaece42e46727c8777d0d7
Thomas Lundqvist <tlundqvist@acm.org>
2021-09-14 19:44:10 +0200

    Added comment

--- get-all-coins.sh
+++ get-all-coins.sh
@@ -1,3 +1,6 @@
+# Example shell script for linux to generate and automatically open
+# an html table using the system default web browser
+
 ./cmc-get.py -html > cmc-get.html
 x-www-browser cmc-get.html
 
